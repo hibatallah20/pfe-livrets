7ef334232d8c9b925d59de65176df41fd734bc03
hibatallah20 <h.bouarfa@esisa.ac.ma>
2025-07-06 22:20:32 +0200

    init

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ import connectDB from './config/db.js'
 import  authRouter  from './routes/authRoutes.js'
 import userRouter from "./routes/userRoutes.js";
 import jobRouter from "./routes/jobRoutes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
 
+import path from 'path'
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -24,5 +30,14 @@ app.get('/', (req, res) => res.send("API Working"));
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 app.use("/api/jobs", jobRouter)
+app.use('/api/resume', resumeRouter)
+app.use(
+    '/uploads',
+    express.static(path.join(__dirname, 'uploads'), {
+        setHeaders: (res, _path) => {
+            res.set('Access-Control-Allow-Origin', 'http://localhost:5173')
+        }
+    })
+)
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
